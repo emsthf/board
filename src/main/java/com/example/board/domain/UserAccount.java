d9eca42d43b5ab5eebc5b2ab5f3ade16a30c778d
emsthf <deunsol916@gmail.com>
2023-10-24 14:15:21 +0900

    #16 - 계정 도메인 추가 작업
    로그인을 위한 계정 도메인 추가

---
+++ src/main/java/com/example/board/domain/UserAccount.java
@@ -0,0 +1,70 @@
+package com.example.board.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "userId"),
+        @Index(columnList = "email", unique = true),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+public class UserAccount extends AuditingFields {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter
+    @Column(nullable = false, length = 50)
+    private String userId;
+
+    @Setter
+    @Column(nullable = false)
+    private String userPassword;
+
+    @Setter
+    @Column(length = 100)
+    private String email;
+
+    @Setter
+    @Column(length = 100)
+    private String nickname;
+
+    @Setter
+    private String meno;
+
+
+    protected UserAccount() {
+    }
+
+    public UserAccount(String userId, String userPassword, String email, String nickname, String meno) {
+        this.userId = userId;
+        this.userPassword = userPassword;
+        this.email = email;
+        this.nickname = nickname;
+        this.meno = meno;
+    }
+
+    public static UserAccount of(String userId, String userPassword, String email, String nickname, String meno) {
+        return new UserAccount(userId, userPassword, email, nickname, meno);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof UserAccount that)) return false;
+        return id != null && Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
